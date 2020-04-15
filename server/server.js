6636728a341bd86558c323216858a84d8e6b0711
MohanedKassim <aliyyi89@gmail.com>
2020-04-15 14:45:25 -0500

    initial file setup

---
+++ server/server.js
@@ -0,0 +1,12 @@
+//requires
+const express = require('express');
+const app = express();
+//globals
+const port = 5000;
+//uses
+app.use(express.static('server/public'));
+//spin up server
+app.listen(port, () =>{
+    console.log('server is up on:', port)
+})//end server up
+//get route
